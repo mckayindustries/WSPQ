d1522f1728d4ecc8f07c883b7f8df635378eac97
Jeff Guillou <jean.f.guillou@gmail.com>
2015-01-22 11:42:23 +0100

    Added printerWatcher snmp calls to make sure pageCount is correct

--- WSPQ/PrintMonitor.cs
+++ WSPQ/PrintMonitor.cs
@@ -17,7 +17,8 @@ namespace WSPQ
         private int printCount = 0;
         private int deleteCount = 0;
         private HttpWrapper http;
-        
+        private PrinterWatcher watcher;
+
         public void MonitorAllPrinters()
         {
             String[] printers = GetMonitorablePrinters();
@@ -30,27 +31,39 @@ namespace WSPQ
 
             Console.WriteLine(".Start");
             PrinterMonitorComponent pMon = new PrinterMonitorComponent();
+            pMon.MonitorJobSetEvent = false;
+
             http = new HttpWrapper();
+            watcher = new PrinterWatcher(http);
 
             foreach (string printer in printers)
             {
+                Console.WriteLine(printer);
                 pMon.AddPrinter(printer);
+                watcher.AddPrinter(printer, pMon.get_PrinterInformation(printer));
             }
 
+            // JobAdded may not fire correctly, assume it'll be written at some point before deletion
             pMon.JobAdded += this.OnJobUpdate;
             pMon.JobWritten += this.OnJobUpdate;
             pMon.JobDeleted += this.OnJobDelete;
+            watcher.JobEnded += this.OnJobEnded;
 
+            watcher.Start();
             log = new StreamWriter("printjob.log", true, Encoding.Default);
             log.AutoFlush = true;
             log.WriteLine("Started @ {0}", DateTime.Now.ToString());
 
             Console.WriteLine(".Monit");
             Console.ReadKey();
-            log.WriteLine("Ended after {0} prints & {1} cancels @ {2}", printCount, deleteCount, DateTime.Now.ToString());
-            log.WriteLine();
-            log.Close();
+            lock (logSyncLock)
+            {
+                log.WriteLine("Ended after {0} prints & {1} cancels @ {2}", printCount, deleteCount, DateTime.Now.ToString());
+                log.WriteLine();
+                log.Close();
+            }
             Console.WriteLine(".Ended");
+            watcher.Stop();
             pMon.Disconnect();
         }
 
@@ -70,6 +83,13 @@ namespace WSPQ
             lock (logSyncLock) { log.WriteLine(output); }
 
             int pages = j.TotalPages > 0 ? j.TotalPages : 1; // Assume 1 page when spooler says 0
+
+            if (watcher.EnqueueAwaitingJob(j))
+            {
+                Console.WriteLine(String.Format("Printer {0} : Added job {1}", j.PrinterName, j.JobId));
+                j.Paused = true;
+            }
+
             if (!http.CanPrint(j.UserName, pages * j.Copies))
                 DeleteJob(j);
         }
@@ -88,7 +108,16 @@ namespace WSPQ
                     e.EventTime, e.EventType, j.JobId, j.PrinterName, j.UserName,
                     j.Document, j.Copies, j.PagesPrinted, j.TotalPages);
             lock (logSyncLock) { log.WriteLine(output); }
+
             http.HasPrinted(j.JobId, j.UserName, j.PrinterName, j.Document, j.PagesPrinted);
+        }
+
+        private void OnJobEnded(object sender, PrintJobEndArgs e)
+        {
+            Console.WriteLine(String.Format("OnJobEnded : {0} {1} => {2}", e.jobId, e.pageCount, e.truePageCount));
+            //e.jobId;
+            //e.pageCount;
+            //update pagecount if necessary
         }
 
         private void DeleteJob(PrintJob j)
@@ -107,6 +136,7 @@ namespace WSPQ
 
         protected String[] GetMonitorablePrinters()
         {
+            // Remove unmonitorable printers
             String[] ignoredPrinters = {
                 "PDFCreator",
                 "Microsoft XPS Document Writer",
---
+++ WSPQ/PrinterWatcher.cs
@@ -0,0 +1,204 @@
+﻿using PrinterQueueWatch;
+using SnmpSharpNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WSPQ
+{
+    class PrinterWatcher
+    {
+        private bool threadRunning = true;
+        private Thread t;
+        private HttpWrapper http;
+        private Dictionary<String, WatchedPrinter> watchedPrinters;
+        public delegate void JobEndedEvent(PrinterWatcher w, PrintJobEndArgs e);
+        public event JobEndedEvent JobEnded;
+
+        public PrinterWatcher(HttpWrapper http)
+        {
+            watchedPrinters = new Dictionary<String, WatchedPrinter>();
+            this.http = http;
+            t = new Thread(new ThreadStart(ThreadLoop));
+        }
+
+        public void AddPrinter(string name, PrinterInformation printerInfo)
+        {
+            watchedPrinters.Add(name, new WatchedPrinter(name, ShareToAddress(printerInfo.ShareName)));
+        }
+
+        public void Start()
+        {
+            t.Start();
+        }
+
+        public void Stop()
+        {
+            threadRunning = false;
+        }
+
+        public void ThreadLoop()
+        {
+            while (threadRunning)
+            {
+                foreach (KeyValuePair<String, WatchedPrinter> kv in watchedPrinters)
+                {
+                    if (!threadRunning)
+                        break;
+
+                    WatchedPrinter p = kv.Value;
+
+                    if (p.awaitingJobs.Count == 0) // Queue is empty
+                        continue;
+
+                    if (!p.UpdateStatusAndPageCount())
+                    {
+                        Console.WriteLine(String.Format("Printer {0} is likely offline ?", p.name));
+                        continue;
+                    }
+                    Console.WriteLine(String.Format("Printer {0} is {1}[{2}] with {3} job ({4}/{5})", p.name, p.isReady ? "ready" : "busy", p.status, p.awaitingJobs.Count, p.runningJobId, p.isSpittingPaper ? "spit" : ""));
+
+                    if (p.isReady && !p.hasRunningJob) // Nothing running and printer is ready, let's print
+                    {
+                        p.RunFirstAwaitingJob();
+                        Console.WriteLine(String.Format("Printer {0} : RunJob ({1})", p.name, p.runningJobId));
+                    }
+
+                    if (p.hasRunningJob && !p.isReady) // Job is running and printer is doing something, aknowledge printing
+                    {
+                        //Console.WriteLine(String.Format("Printer {0} : spitPaper ({1})", p.name, p.runningJobId));
+                        p.isSpittingPaper = true;
+                    }
+
+                    if (p.hasRunningJob && p.isReady && p.isSpittingPaper) // Printer was running our job, spat papper and now is ready
+                    {
+                        Console.WriteLine(String.Format("Printer {0} : Done ({1})", p.name, p.runningJobId));
+                        p.isSpittingPaper = false;
+
+                        PrintJobEndArgs e = new PrintJobEndArgs();
+                        e.jobId = p.runningJobId;
+                        e.truePageCount = p.pageCount - p.pageCountBefore;
+                        e.pageCount = p.currentPrintJob.PagesPrinted;
+                        JobEnded(this, e);
+
+                        p.JobEnded();
+                    }
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        public bool EnqueueAwaitingJob(PrintJob job)
+        {
+            WatchedPrinter printer;
+            if (watchedPrinters.TryGetValue(job.PrinterName, out printer))
+            {
+                return printer.AddAwaitingJob(job);
+            }
+            return false;
+        }
+
+        private string ShareToAddress(string shareName)
+        {
+            return String.Format("{0}.{1}.{2}", shareName[1] == 'E' ? "148.60" : "129.20", shareName.Substring(2, 3).TrimStart('0'), shareName.Substring(5, 3).TrimStart('0'));
+        }
+    }
+
+    class WatchedPrinter
+    {
+        private static string PAGECOUNT_OID = "1.3.6.1.2.1.43.10.2.1.4.1.1";
+        private static string STATUS_OID = "1.3.6.1.2.1.25.3.5.1.1.1";
+        
+        public string name;
+        public string address;
+        private SimpleSnmp snmp;
+        public int status;
+        public bool isReady = true;
+
+        public Dictionary<int, PrintJob> awaitingJobs;
+        public PrintJob currentPrintJob;
+        public int pageCount;
+        public int pageCountBefore;
+        public bool hasRunningJob = false;
+        public int runningJobId = 0;
+        public bool isSpittingPaper = false;
+
+        public WatchedPrinter(string name, string address)
+        {
+            this.name = name;
+            this.address = address;
+            snmp = new SimpleSnmp(address, "public");
+            awaitingJobs = new Dictionary<int, PrintJob>();
+        }
+
+        public bool AddAwaitingJob(PrintJob job)
+        {
+            if (!awaitingJobs.ContainsKey(job.JobId))
+            {
+                awaitingJobs.Add(job.JobId, job);
+                return true;
+            }
+            return false;
+        }
+
+        public void RunFirstAwaitingJob()
+        {
+            KeyValuePair<int, PrintJob> kv = awaitingJobs.First();
+            currentPrintJob = kv.Value;
+            currentPrintJob.Paused = false;
+            runningJobId = kv.Key;
+
+            hasRunningJob = true;
+            pageCountBefore = pageCount;
+        }
+
+        public void JobEnded()
+        {
+            awaitingJobs.Remove(runningJobId);
+            runningJobId = 0;
+            currentPrintJob = null;
+            hasRunningJob = false;
+        }
+
+        public bool UpdateStatusAndPageCount()
+        {
+            Dictionary<Oid, AsnType> res = snmp.Get(SnmpVersion.Ver1, new String[] { STATUS_OID, PAGECOUNT_OID });
+
+            if (res != null)
+            {
+                foreach (KeyValuePair<Oid, AsnType> kv in res)
+                {
+                    string oid = kv.Key.ToString();
+                    if (oid == STATUS_OID)
+                    {
+                        status = int.Parse(kv.Value.ToString());
+                        isReady = status == SnmpStatus.IDLE;
+                    }
+                    else if (oid == PAGECOUNT_OID)
+                        pageCount = int.Parse(kv.Value.ToString());
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+
+    class PrintJobEndArgs : EventArgs
+    {
+        public int jobId;
+        public int truePageCount;
+        public int pageCount;
+    }
+
+    class SnmpStatus
+    {
+        public static int OTHER = 1;
+        public static int UNKNOWN = 2;
+        public static int IDLE = 3;
+        public static int PRINTING = 4;
+        public static int WARMUP = 5;
+    }
+}
